d8fb96ef9962dcce60128ceb49a87df4f836d06f
W0u43r <w.b.ewalts@gmail.com>
2026-05-29 16:17:32 +0200

    Update scores.js

--- api/scores.js
+++ api/scores.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const r = await fetch(
-    'https://api.football-data.org/v4/competitions/2000/matches?status=LIVE,FINISHED,SCHEDULED',
+    'https://api.football-data.org/v4/competitions/2021/matches?status=LIVE,FINISHED,SCHEDULED',
     { headers: { 'X-Auth-Token': '55228a28932e43939447a577a695fada' } }
   );
   const data = await r.json();
